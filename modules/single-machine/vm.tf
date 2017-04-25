c26ea0b9e2cab9f5be6d676e75cbf6240738af72
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-04-25 18:37:56 +1000

    Using first provisioner

--- modules/single-machine/vm.tf
+++ modules/single-machine/vm.tf
@@ -25,20 +25,26 @@ resource "openstack_compute_instance_v2" "vm" {
     uuid = "${data.terraform_remote_state.base.network-id}"
   }
 
-  # provisioner "remote-exec" {
-  #   connection {
-  #     user        = "${var.ssh_username}"
-  #     private_key = "${file(var.ssh_key_file)}"
-  #   }
-  #
-  #   inline = [
-  #     "apt-get -y update",
-  #     "apt-get -y upgrade",
-  #   ]
-  # }
+
 }
 
 resource "openstack_compute_floatingip_associate_v2" "fip_vm" {
   floating_ip = "${openstack_compute_floatingip_v2.ip.address}"
   instance_id = "${openstack_compute_instance_v2.vm.id}"
+}
+
+resource "null_resource" "provision" {
+  depends_on = ["openstack_compute_floatingip_associate_v2.fip_vm"]
+  connection {
+    user        = "${var.ssh_username}"
+    private_key = "${file(var.ssh_key_file)}"
+    host        = "${openstack_compute_floatingip_v2.ip.address}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get -y update",
+      "apt-get -o Dpkg::Options::=\"--force-confold\" --force-yes -y upgrade",
+    ]
+  }
 }
