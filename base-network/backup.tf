8114774ecc94287a7572d49b40a3d95745d8c869
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-05-08 23:10:21 +1000

    Add both backups

--- base-network/backup.tf
+++ base-network/backup.tf
@@ -1,3 +1,34 @@
 resource "aws_s3_bucket" "automatic-backups" {
   bucket = "openmrs-backups"
+  lifecycle_rule {
+    id      = "archive-and-delete"
+    prefix  = ""
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+    expiration {
+      days = 180
+    }
+  }
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket" "manual-backups" {
+  bucket = "openmrs-backups-manual"
+  lifecycle_rule {
+    id      = "archive-and-delete"
+    prefix  = ""
+    enabled = true
+    transition {
+      days          = 180
+      storage_class = "GLACIER"
+    }
+  }
+  versioning {
+    enabled = true
+  }
 }
