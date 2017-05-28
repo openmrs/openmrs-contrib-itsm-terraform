ae04453a951474ab33368164b21b4151a40c23cf
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-05-28 18:39:52 +1000

    Adding garissa

--- garissa/main.tf
+++ garissa/main.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "openmrs-terraform-state-files"
-    key    = "thika.tfstate"
+    key    = "garissa.tfstate"
   }
 }
 
@@ -29,9 +29,36 @@ module "single-machine" {
   use_ansible       = true
 }
 
-resource "dme_record" "quizgrader" {
+resource "dme_record" "demo" {
   domainid    = "${var.domain_dns["openmrs.org"]}"
-  name        = "quizgrader"
+  name        = "demo"
+  type        = "CNAME"
+  value       = "${var.hostname}"
+  ttl         = 3600
+  gtdLocation = "DEFAULT"
+}
+
+resource "dme_record" "modules" {
+  domainid    = "${var.domain_dns["openmrs.org"]}"
+  name        = "modules-refapp"
+  type        = "CNAME"
+  value       = "${var.hostname}"
+  ttl         = 3600
+  gtdLocation = "DEFAULT"
+}
+
+resource "dme_record" "uat" {
+  domainid    = "${var.domain_dns["openmrs.org"]}"
+  name        = "uat-refapp"
+  type        = "CNAME"
+  value       = "${var.hostname}"
+  ttl         = 3600
+  gtdLocation = "DEFAULT"
+}
+
+resource "dme_record" "qa" {
+  domainid    = "${var.domain_dns["openmrs.org"]}"
+  name        = "qa-refapp"
   type        = "CNAME"
   value       = "${var.hostname}"
   ttl         = 3600
---
+++ garissa/variables.tf
@@ -0,0 +1,11 @@
+variable "flavor" {
+  default = "m1.small"
+}
+
+variable "hostname" {
+  default = "garissa"
+}
+
+variable "ansible_inventory" {
+  default = "production"
+}
