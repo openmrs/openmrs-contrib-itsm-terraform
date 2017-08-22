8f75eaf97e2b4da3114cc1e2a5e96cda05a3f72d
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-08-22 21:20:13 +1000

    ITSM-4018: Add buckets and users for ocl

---
+++ nairobi/ocl-resources.tf
@@ -0,0 +1,46 @@
+resource "aws_s3_bucket" "ocl-bucket" {
+  bucket = "openmrs-ocl-prd"
+  versioning {
+    enabled = true
+  }
+  tags {
+    Terraform        = "${var.hostname}"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_iam_user" "ocl-user" {
+  name  = "ocl-prd-${var.hostname}"
+}
+
+resource "aws_iam_access_key" "ocl-user-key" {
+  user = "${aws_iam_user.ocl-user.name}"
+}
+
+resource "aws_iam_user_policy" "ocl-user-policy" {
+  name  = "ocl_prd"
+  user  = "${aws_iam_user.ocl-user.name}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["${aws_s3_bucket.ocl-bucket.arn}"]
+    },
+    {
+      "Action": [
+        "s3:PutObject",
+        "s3:ListMultipartUploadParts",
+        "s3:AbortMultipartUpload"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.ocl-bucket.arn}/*"
+    }
+  ]
+}
+EOF
+}
