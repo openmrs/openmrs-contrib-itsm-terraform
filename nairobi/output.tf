b8f9e0c6d7ca8835493c880a08f1e0c494a8b73b
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-09-04 13:29:27 +1000

    ITSM-3960: create output for machines with backup

--- nairobi/output.tf
+++ nairobi/output.tf
@@ -1,7 +1,15 @@
-output "user_access_key_id" {
+output "ocl_access_key_id" {
   value = "${aws_iam_access_key.ocl-user-key.id}"
 }
 
-output "user_access_key_secret" {
+output "ocl_access_key_secret" {
   value = "${aws_iam_access_key.ocl-user-key.secret}"
+}
+
+output "backup_access_key_id" {
+  value = "${module.single-machine.user_access_key_id}"
+}
+
+output "backup_access_key_secret" {
+  value = "${module.single-machine.user_access_key_secret}"
 }
