4be034e12a332f2cdd3b29e70efcee1193faa92c
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-09-24 16:56:54 +1000

    ITSM-4014: fix dns for jetstream; second part

--- conf/provisioning/configure-dns.sh
+++ conf/provisioning/configure-dns.sh
@@ -6,6 +6,7 @@ set -v
 
 sed -i -r "s/dns=dnsmasq/dns=none/" /etc/NetworkManager/NetworkManager.conf
 pkill dnsmasq || true
+rm -rf /var/run/resolvconf/interface/NetworkManager
 
 systemctl restart NetworkManager
 systemctl restart networking
