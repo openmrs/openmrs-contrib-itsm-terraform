874c61074f0806d8c60b8ab6047bd8a48ff7866a
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-10-15 19:36:21 +1100

    ITSM-4049: add cloudfront certificate and docs dns entries

--- docs/main.tf
+++ docs/main.tf
@@ -50,6 +50,69 @@ POLICY
   }
 }
 
+
+resource "aws_cloudfront_distribution" "cloudfront_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.docs-s3.bucket_domain_name}"
+    origin_id = "S3-${var.bucket_name}"
+  }
+  enabled = true
+  aliases = ["docs.openmrs.org", "resources.openmrs.org"]
+  logging_config {
+    include_cookies = false
+    bucket          = "${aws_s3_bucket.log_bucket.bucket_domain_name}"
+    prefix          = "cloudfront"
+  }
+  tags {
+    Terraform        = "docs"
+  }
+  default_cache_behavior {
+    allowed_methods = [ "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT" ]
+    cached_methods = [ "GET", "HEAD" ]
+    target_origin_id = "S3-${var.bucket_name}"
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl = 0
+    default_ttl = 300
+    max_ttl = 3600
+  }
+  viewer_certificate {
+    acm_certificate_arn = "${var.certificate_arn}"
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1"
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
+resource "dme_record" "docs" {
+  domainid    = "${var.domain_dns["openmrs.org"]}"
+  name        = "docs"
+  type        = "CNAME"
+  value       = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}"
+  ttl         = 3600
+  gtdLocation = "DEFAULT"
+}
+
+resource "dme_record" "resources" {
+  domainid    = "${var.domain_dns["openmrs.org"]}"
+  name        = "resources"
+  type        = "CNAME"
+  value       = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}"
+  ttl         = 3600
+  gtdLocation = "DEFAULT"
+}
+
+
 resource "aws_iam_user" "bamboo-user" {
   name  = "bamboo-docs-upload"
 }
@@ -82,36 +145,4 @@ resource "aws_iam_user_policy" "bamboo-user-policy" {
   ]
 }
 EOF
-}
-
-
-resource "aws_cloudfront_distribution" "cloudfront_distribution" {
-  origin {
-    domain_name = "${var.bucket_name}.s3.amazonaws.com"
-    origin_id = "S3-${var.bucket_name}"
-  }
-  enabled = true
-  default_cache_behavior {
-    allowed_methods = [ "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT" ]
-    cached_methods = [ "GET", "HEAD" ]
-    target_origin_id = "S3-${var.bucket_name}"
-    forwarded_values {
-      query_string = true
-      cookies {
-        forward = "none"
-      }
-    }
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl = 0
-    default_ttl = 300
-    max_ttl = 3600
-  }
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
 }
