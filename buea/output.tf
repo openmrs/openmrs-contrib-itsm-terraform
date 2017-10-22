0af5547b8d1b0da5affe6d30e88e2d154f29b45a
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-10-22 16:58:35 +1100

    Create new msf and new mavenrepo redirect

---
+++ buea/output.tf
@@ -0,0 +1,8 @@
+
+output "backup_access_key_id" {
+  value = "${module.single-machine.user_access_key_id}"
+}
+
+output "backup_access_key_secret" {
+  value = "${module.single-machine.user_access_key_secret}"
+}
