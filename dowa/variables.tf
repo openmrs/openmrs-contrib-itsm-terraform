04ea8f41a3d79e29848d86b9d1ef82fa431898c7
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-11-08 20:00:50 +1100

    Add dowa and lobi

---
+++ dowa/variables.tf
@@ -0,0 +1,42 @@
+# Description of arguments can be found in
+# ../modules/single-machine/variables.tf in this repository
+
+variable "flavor" {
+  default = "m1.small"
+}
+
+variable "region" {
+  default = "iu"
+}
+
+variable "hostname" {
+  default = "dowa"
+}
+
+variable "update_os" {
+  default = true
+}
+
+variable "use_ansible" {
+  default = false
+}
+
+variable "ansible_inventory" {
+  default = "testing"
+}
+
+variable "has_data_volume" {
+  default = false
+}
+
+variable "data_volume_size" {
+  default = 20
+}
+
+variable "has_backup" {
+  default = true
+}
+
+variable "dns_cnames" {
+  default = ["mdsbuilder-new"]
+}
---
+++ lobi/variables.tf
@@ -0,0 +1,42 @@
+# Description of arguments can be found in
+# ../modules/single-machine/variables.tf in this repository
+
+variable "flavor" {
+  default = "m1.medium"
+}
+
+variable "region" {
+  default = "iu"
+}
+
+variable "hostname" {
+  default = "lobi"
+}
+
+variable "update_os" {
+  default = true
+}
+
+variable "use_ansible" {
+  default = false
+}
+
+variable "ansible_inventory" {
+  default = "production"
+}
+
+variable "has_data_volume" {
+  default = true
+}
+
+variable "data_volume_size" {
+  default = 40
+}
+
+variable "has_backup" {
+  default = true
+}
+
+variable "dns_cnames" {
+  default = ["ci-new"]
+}
