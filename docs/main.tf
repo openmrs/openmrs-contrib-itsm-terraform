06ebc4e6594de56f3b3ac6810b3f12db81347f80
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-11-08 18:15:15 +1100

    Fix content type for vm docs

--- docs/main.tf
+++ docs/main.tf
@@ -156,8 +156,9 @@ EOF
 }
 
 resource "aws_s3_bucket_object" "vms-inventory" {
-  bucket = "${var.bucket_name}"
-  key    = "infrastructure/vms.html"
-  source = "vms.html"                          # use './build docs' to generate it
-  etag   = "${md5(file("vms.html"))}"
+  bucket       = "${var.bucket_name}"
+  key          = "infrastructure/vms.html"
+  source       = "vms.html"                          # use './build docs' to generate it
+  etag         = "${md5(file("vms.html"))}"
+  content_type = "text/html"
 }
