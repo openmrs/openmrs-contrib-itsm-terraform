75c16b409a8b553c41cd0c5140b3c5a4bca5d389
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-12-05 20:08:43 +1100

    Adding machines for sync demo and new sonarqube

---
+++ goura/variables.tf
@@ -0,0 +1,46 @@
+# Description of arguments can be found in
+# ../modules/single-machine/variables.tf in this repository
+
+variable "flavor" {
+  default = "m1.medium"
+}
+
+variable "region" {
+  default = "iu"
+}
+
+variable "hostname" {
+  default = "goura"
+}
+
+variable "update_os" {
+  default = true
+}
+
+variable "use_ansible" {
+  default = false
+}
+
+variable "ansible_inventory" {
+  default = "production"
+}
+
+variable "has_data_volume" {
+  default = true
+}
+
+variable "data_volume_size" {
+  default = 30
+}
+
+variable "has_backup" {
+  default = true
+}
+
+variable "dns_cnames" {
+  default = ["sync1", "sync2", "sync3", "sync4"]
+}
+
+variable "description" {
+  default = ""
+}
