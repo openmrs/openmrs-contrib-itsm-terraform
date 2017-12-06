b6418732148d453923ad33fe945443109e515c34
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-12-06 13:19:23 +1100

    Allow mysql to be accessible from bamboo agents

--- mokolo/main.tf
+++ mokolo/main.tf
@@ -11,6 +11,14 @@ provider "openstack" {
   auth_url = "${var.iu_url}"
 }
 
+data "terraform_remote_state" "base" {
+    backend = "s3"
+    config {
+        bucket = "openmrs-terraform-state-files"
+        key    = "basic-network-setup.tfstate"
+    }
+}
+
 # Description of arguments can be found in
 # ../modules/single-machine/variables.tf in this repository
 module "single-machine" {
@@ -27,6 +35,7 @@ module "single-machine" {
   data_volume_size  = "${var.data_volume_size}"
   has_backup        = "${var.has_backup}"
   dns_cnames        = "${var.dns_cnames}"
+  extra_security_groups = ["${openstack_compute_secgroup_v2.bamboo-remote-agent.name}"]
 
 
   # Global variables
@@ -37,4 +46,52 @@ module "single-machine" {
   ssh_key_file      = "${var.ssh_key_file}"
   domain_dns        = "${var.domain_dns}"
   ansible_repo      = "${var.ansible_repo}"
+}
+
+resource "dme_record" "private-dns" {
+  domainid    = "${var.domain_dns["openmrs.org"]}"
+  name        = "sonar-internal"
+  type        = "A"
+  value       = "${module.single-machine.private_address}"
+  ttl         = 300
+  gtdLocation = "DEFAULT"
+}
+
+
+resource "openstack_compute_secgroup_v2" "bamboo-remote-agent" {
+  name        = "${var.project_name}-bamboo-server-agents-sonar"
+  description = "Allow bamboo agents to connect to mysql sonar server (terraform)."
+
+  # Bamboo agents Jetstream in IU (works with the private IP only)
+  rule {
+    from_port      = 3306
+    to_port        = 3306
+    ip_protocol    = "tcp"
+    from_group_id  = "${data.terraform_remote_state.base.secgroup-bamboo-remote-agent-id-iu}"
+  }
+
+  ##### after migration, the agents shoud connect using the private DNS - ITSM-4090
+  # yak jetstream
+  rule {
+    from_port   = 3306
+    to_port     = 3306
+    ip_protocol = "tcp"
+    cidr        = "149.165.168.253/32"
+  }
+
+  # yokobue jetstream
+  rule {
+    from_port   = 3306
+    to_port     = 3306
+    ip_protocol = "tcp"
+    cidr        = "149.165.169.187/32"
+  }
+
+  # yue jetstream
+  rule {
+    from_port   = 3306
+    to_port     = 3306
+    ip_protocol = "tcp"
+    cidr        = "149.165.168.182/32"
+  }
 }
