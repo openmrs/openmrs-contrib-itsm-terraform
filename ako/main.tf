ab3f8ba88c32489d9078d9427f7bc56e6ced82c8
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-12-08 16:57:16 +1100

    Allow baragoi to connect to ako

--- ako/main.tf
+++ ako/main.tf
@@ -86,6 +86,18 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ldaps_xsede" {
   security_group_id = "${openstack_networking_secgroup_v2.secgroup_ldap.id}"
 }
 
+#bagaroi
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ldaps_id" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 636
+  port_range_max    = 636
+  remote_ip_prefix  = "149.165.168.41/32"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_ldap.id}"
+}
+
+
 resource "dme_record" "private-dns" {
   domainid    = "${var.domain_dns["openmrs.org"]}"
   name        = "ldap-internal"
