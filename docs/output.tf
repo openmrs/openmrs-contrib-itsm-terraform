5de8d17fe604eceee1b33fb775dc7da3190ac753
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2018-01-28 18:22:39 +1100

    ITSM-4101: Added user for website resources

--- docs/output.tf
+++ docs/output.tf
@@ -5,3 +5,12 @@ output "user_access_key_id" {
 output "user_access_key_secret" {
   value = "${aws_iam_access_key.bamboo-user-key.secret}"
 }
+
+
+output "website_user_access_key_id" {
+  value = "${aws_iam_access_key.website-assets-key.id}"
+}
+
+output "website_user_access_key_secret" {
+  value = "${aws_iam_access_key.website-assets-key.secret}"
+}
