e3d82347d040c28c4741bd5cde685d7ea78bb739
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2018-01-28 17:47:03 +1100

    Adding new talk serve

---
+++ likoma/variables.tf
@@ -0,0 +1,46 @@
+# Description of arguments can be found in
+# ../modules/single-machine/variables.tf in this repository
+
+variable "flavor" {
+  default = "m1.medium"
+}
+
+variable "region" {
+  default = "iu"
+}
+
+variable "hostname" {
+  default = "likoma"
+}
+
+variable "update_os" {
+  default = true
+}
+
+variable "use_ansible" {
+  default = true
+}
+
+variable "ansible_inventory" {
+  default = "prod-tier1"
+}
+
+variable "has_data_volume" {
+  default = true
+}
+
+variable "data_volume_size" {
+  default = 60
+}
+
+variable "has_backup" {
+  default = true
+}
+
+variable "dns_cnames" {
+  default = ["talk-new"]
+}
+
+variable "description" {
+  default = "Discourse/forum server"
+}
