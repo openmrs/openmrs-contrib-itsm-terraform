6a328680b6dae508f043991717fe4eb9c609ad21
Cintia Del Rio <cintia@delrio.im>
2020-11-14 15:56:14 +1100

    ITSM-4285: allowing ubuntu 20 and 16 to coexist here

--- conf/provisioning/configure-dns.sh
+++ conf/provisioning/configure-dns.sh
@@ -4,7 +4,7 @@ set -u
 set -e
 set -v
 
-sed -i -r "s/dns=dnsmasq/dns=none/" /etc/NetworkManager/conf.d/NetworkManager.conf
+sed -i -r "s/dns=dnsmasq/dns=none/" /etc/NetworkManager/NetworkManager.conf
 pkill dnsmasq || true
 rm -rf /var/run/resolvconf/interface/NetworkManager
 
