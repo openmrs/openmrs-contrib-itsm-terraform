7a28893d65fe5cae4d9d59964427a84487aaf9a9
PermissionError <dev@rayma.me>
2021-02-01 18:23:25 +1300

    ITSM-4290: Provision namanga

---
+++ namanga/variables.tf
@@ -0,0 +1,46 @@
+# Description of arguments can be found in
+# ../modules/single-machine/variables.tf in this repository
+
+variable "flavor" {
+  default = "m1.medium"
+}
+ 
+variable "region" {
+  default = "iu"
+}
+
+variable "hostname" {
+  default = "namanga"
+}
+
+variable "update_os" {
+  default = true
+}
+
+variable "use_ansible" {
+  default = false
+}
+
+variable "ansible_inventory" {
+  default = "prod-tier4"
+}
+
+variable "has_data_volume" {
+  default = true
+}
+
+variable "data_volume_size" {
+  default = 20
+}
+
+variable "has_backup" {
+  default = true
+}
+
+variable "dns_cnames" {
+  default = ["openhim","hapifhir","opencr"]
+}
+
+variable "description" {
+  default = "PLIR PoC Test Servers"
+}
