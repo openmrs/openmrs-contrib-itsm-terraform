276dd905181f63f82b01e4d3a6cbd4832d9ab054
Cintia Del Rio <cintia@delrio.im>
2021-09-05 17:04:59 +1000

    Probably all ugpraded to terraform 12

--- docs/main.tf
+++ docs/main.tf
@@ -6,24 +6,16 @@ terraform {
   }
 }
 
-provider "dme" {
-    version = "0.1.0"
-}
-
-provider "null" {
-    version = "1.0.0"
-}
-
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "openmrs-docs-logs"
   acl    = "log-delivery-write"
-  tags {
-    Terraform        = "docs"
+  tags = {
+    Terraform = "docs"
   }
 }
 
 resource "aws_s3_bucket" "docs-s3" {
-  bucket = "${var.bucket_name}"
+  bucket = var.bucket_name
   acl    = "public-read"
   policy = <<POLICY
 {
@@ -39,30 +31,31 @@ resource "aws_s3_bucket" "docs-s3" {
   ]
 }
 POLICY
+
+
   website {
     index_document = "index.html"
     error_document = "error.html"
   }
   logging {
-    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+    target_bucket = aws_s3_bucket.log_bucket.id
     target_prefix = "log/"
   }
   versioning {
     enabled = true
   }
-  tags {
-    Terraform        = "docs"
+  tags = {
+    Terraform = "docs"
   }
   lifecycle {
     prevent_destroy = false
   }
 }
 
-
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   origin {
-    domain_name          = "${aws_s3_bucket.docs-s3.website_endpoint}"
-    origin_id            = "S3-${var.bucket_name}"
+    domain_name = aws_s3_bucket.docs-s3.website_endpoint
+    origin_id   = "S3-${var.bucket_name}"
     custom_origin_config {
       origin_read_timeout    = 60
       http_port              = 80
@@ -71,20 +64,20 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
-  enabled = true
-  aliases = ["docs.openmrs.org", "resources.openmrs.org"]
+  enabled             = true
+  aliases             = ["docs.openmrs.org", "resources.openmrs.org"]
   default_root_object = "index.html"
   logging_config {
     include_cookies = false
-    bucket          = "${aws_s3_bucket.log_bucket.bucket_domain_name}"
+    bucket          = aws_s3_bucket.log_bucket.bucket_domain_name
     prefix          = "cloudfront"
   }
-  tags {
-    Terraform        = "docs"
+  tags = {
+    Terraform = "docs"
   }
   default_cache_behavior {
-    allowed_methods = [ "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT" ]
-    cached_methods = [ "GET", "HEAD" ]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3-${var.bucket_name}"
 
     forwarded_values {
@@ -94,13 +87,13 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
       }
     }
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl = 0
-    default_ttl = 300
-    max_ttl = 3600
+    min_ttl                = 0
+    default_ttl            = 300
+    max_ttl                = 3600
   }
   viewer_certificate {
-    acm_certificate_arn = "${var.certificate_arn}"
-    ssl_support_method = "sni-only"
+    acm_certificate_arn      = var.certificate_arn
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1"
   }
   restrictions {
@@ -110,36 +103,33 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   }
 }
 
-resource "dme_record" "docs" {
-  domainid    = "${var.domain_dns["openmrs.org"]}"
-  name        = "docs"
-  type        = "CNAME"
-  value       = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}."
-  ttl         = 3600
-  gtdLocation = "DEFAULT"
+resource "dme_dns_record" "docs" {
+  domain_id = var.domain_dns["openmrs.org"]
+  name      = "docs"
+  type      = "CNAME"
+  value     = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}."
+  ttl       = 3600
 }
 
-resource "dme_record" "resources" {
-  domainid    = "${var.domain_dns["openmrs.org"]}"
-  name        = "resources"
-  type        = "CNAME"
-  value       = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}."
-  ttl         = 3600
-  gtdLocation = "DEFAULT"
+resource "dme_dns_record" "resources" {
+  domain_id = var.domain_dns["openmrs.org"]
+  name      = "resources"
+  type      = "CNAME"
+  value     = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}."
+  ttl       = 3600
 }
-
 
 resource "aws_iam_user" "bamboo-user" {
-  name  = "bamboo-docs-upload"
+  name = "bamboo-docs-upload"
 }
 
 resource "aws_iam_access_key" "bamboo-user-key" {
-  user = "${aws_iam_user.bamboo-user.name}"
+  user = aws_iam_user.bamboo-user.name
 }
 
 resource "aws_iam_user_policy" "bamboo-user-policy" {
-  name  = "upload_docs"
-  user  = "${aws_iam_user.bamboo-user.name}"
+  name   = "upload_docs"
+  user   = aws_iam_user.bamboo-user.name
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -161,20 +151,22 @@ resource "aws_iam_user_policy" "bamboo-user-policy" {
   ]
 }
 EOF
+
 }
 
 resource "aws_s3_bucket_object" "vms-inventory-json" {
-  bucket       = "${var.bucket_name}"
+  bucket       = var.bucket_name
   key          = "infrastructure/vms.json"
-  source       = "vms.json"                          # use './build docs' to generate it
-  etag         = md5(file("vms.json"))
+  source       = "vms.json" # use './build docs' to generate it
+  etag         = filemd5("vms.json")
   content_type = "application/json"
 }
 
 resource "aws_s3_bucket_object" "vms-inventory" {
-  bucket       = "${var.bucket_name}"
+  bucket       = var.bucket_name
   key          = "infrastructure/vms.html"
-  source       = "vms.html"                        # use './build docs' to generate it
-  etag         = md5(file("vms.html"))
+  source       = "vms.html" # use './build docs' to generate it
+  etag         = filemd5("vms.html")
   content_type = "text/html"
 }
+
