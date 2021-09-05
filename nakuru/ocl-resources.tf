6791b25823a395c78d93cc70eaf3439b34a6c20b
Cintia Del Rio <cintia@delrio.im>
2021-09-05 13:37:58 +1000

    Nakuru on terraform 12

--- nakuru/ocl-resources.tf
+++ nakuru/ocl-resources.tf
@@ -5,8 +5,8 @@ resource "aws_s3_bucket" "ocl-bucket" {
   versioning {
     enabled = true
   }
-  tags {
-    Terraform        = "${var.hostname}"
+  tags = {
+    Terraform        = var.hostname
   }
   lifecycle {
     prevent_destroy = true
@@ -18,35 +18,35 @@ resource "aws_iam_user" "ocl-user" {
 }
 
 resource "aws_iam_access_key" "ocl-user-key" {
-  user = "${aws_iam_user.ocl-user.name}"
+  user = aws_iam_user.ocl-user.name
 }
 
 resource "aws_iam_user_policy" "ocl-user-policy" {
   name  = "ocl_stg"
-  user  = "${aws_iam_user.ocl-user.name}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  user  = aws_iam_user.ocl-user.name
+  policy = jsonencode(
     {
-      "Effect": "Allow",
-      "Action": ["s3:ListBucket"],
-      "Resource": ["${aws_s3_bucket.ocl-bucket.arn}"]
-    },
-    {
-      "Action": [
-        "s3:PutObject*",
-        "s3:ListMultipartUploadParts",
-        "s3:AbortMultipartUpload",
-        "s3:DeleteObject*",
-        "s3:GetObject*"
-      ],
-      "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.ocl-bucket.arn}/*"
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": ["s3:ListBucket"],
+          "Resource": ["${aws_s3_bucket.ocl-bucket.arn}"]
+        },
+        {
+          "Action": [
+            "s3:PutObject*",
+            "s3:ListMultipartUploadParts",
+            "s3:AbortMultipartUpload",
+            "s3:DeleteObject*",
+            "s3:GetObject*"
+          ],
+          "Effect": "Allow",
+          "Resource": "${aws_s3_bucket.ocl-bucket.arn}/*"
+        }
+      ]
     }
-  ]
-}
-EOF
+  )
 }
 
 
@@ -56,8 +56,8 @@ resource "aws_s3_bucket" "ocl-bucket-adhoc" {
   versioning {
     enabled = true
   }
-  tags {
-    Terraform        = "${var.hostname}"
+  tags = {
+    Terraform        = var.hostname
   }
   lifecycle {
     prevent_destroy = true
@@ -69,33 +69,32 @@ resource "aws_iam_user" "ocl-user-adhoc" {
 }
 
 resource "aws_iam_access_key" "ocl-user-key-adhoc" {
-  user = "${aws_iam_user.ocl-user-adhoc.name}"
+  user = aws_iam_user.ocl-user-adhoc.name
 }
 
 resource "aws_iam_user_policy" "ocl-user-policy-adhoc" {
   name  = "ocl_adhoc"
-  user  = "${aws_iam_user.ocl-user-adhoc.name}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": ["s3:ListBucket"],
-      "Resource": ["${aws_s3_bucket.ocl-bucket-adhoc.arn}"]
-    },
-    {
-      "Action": [
-        "s3:PutObject*",
-        "s3:ListMultipartUploadParts",
-        "s3:AbortMultipartUpload",
-        "s3:DeleteObject*",
-        "s3:GetObject*"
-      ],
-      "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.ocl-bucket-adhoc.arn}/*"
-    }
-  ]
-}
-EOF
+  user  = aws_iam_user.ocl-user-adhoc.name
+  policy = jsonencode(
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": ["s3:ListBucket"],
+        "Resource": ["${aws_s3_bucket.ocl-bucket-adhoc.arn}"]
+      },
+      {
+        "Action": [
+          "s3:PutObject*",
+          "s3:ListMultipartUploadParts",
+          "s3:AbortMultipartUpload",
+          "s3:DeleteObject*",
+          "s3:GetObject*"
+        ],
+        "Effect": "Allow",
+        "Resource": "${aws_s3_bucket.ocl-bucket-adhoc.arn}/*"
+      }
+    ]
+  })
 }
