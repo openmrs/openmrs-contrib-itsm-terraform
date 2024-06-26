9ec29a7cff8b5a7a08300e3ab436d9f56f8a7a05
scott45 <busingescott@gmail.com>
2024-06-26 14:15:11 +0300

    try another provider block

--- provider.tf
+++ provider.tf
@@ -40,32 +40,36 @@
 
 
 terraform {
-  required_version = "~> 0.13.0"
-  required_providers {
-    dme = {
-      version = "0.1.3"
-      api_key    = var.dme_apikey
-      secret_key = var.dme_secretkey
-    }
-    aws = {
-      version     = "3.57.0"
-    }
-    template = {
-      version = "2.2"
-    }
-    null = {
-      version = "3.0.0"
-      max_retries = 100
-    }
-    openstack = {
-      version = "1.43"
-    }
-  }
+  required_version = ">= 0.12"
 }
 
 provider "dme" {
-  # dme Api key
+  version = "0.1.3"
   api_key    = var.dme_apikey
-  # dme secret key
   secret_key = var.dme_secretkey
 }
+
+provider "aws" {
+  version     = "3.57.0"
+  max_retries = 100
+  skip_get_ec2_platforms = true
+}
+
+provider "template" {
+  version = "2.2"
+}
+
+provider "null" {
+  version = "3.0.0"
+}
+
+provider "openstack" {
+  version = "1.43"
+}
+
+# provider "dme" {
+#   # dme Api key
+#   api_key    = var.dme_apikey
+#   # dme secret key
+#   secret_key = var.dme_secretkey
+# }
