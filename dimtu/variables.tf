689758f2a8da53d93e03cee9cb611da7b5c3a39d
Scott <busingescott@gmail.com>
2024-08-28 15:37:31 +0300

    Provision new dimtu machine (#26)

---
+++ dimtu/variables.tf
@@ -0,0 +1,53 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Description of arguments can be found in
+# ../modules/single-machine/variables.tf in this repository
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "flavor" {
+  default = "m3.quad"
+}
+
+variable "region" {
+  default = "v2"
+}
+
+variable "hostname" {
+  default = "dimtu"
+}
+
+variable "update_os" {
+  default = true
+}
+
+variable "use_ansible" {
+  default = true
+}
+
+variable "ansible_inventory" {
+  default = "prod-tier4"
+}
+
+variable "has_data_volume" {
+  default = true
+}
+
+variable "data_volume_size" {
+  default = 40
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# If enabling this, make sure to uncomment the two outputs related to backup
+# Those outputs will be copied over manually to ansible, and stored encrypted
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "has_backup" {
+  default = true
+}
+
+variable "dns_cnames" {
+  default = ["dev-his"]
+}
+
+variable "description" {
+  default = "HIS Server"
+}
