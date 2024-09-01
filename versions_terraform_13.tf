3ff58012b8249ae5bbd0d4d63958956e6b9a0456
Cintia Del Rio <cintia@delrio.im>
2024-09-01 18:52:00 +1000

    INFRA-8: following output from 0.13upgrade command

--- versions_terraform_13.tf
+++ versions_terraform_13.tf
@@ -5,35 +5,34 @@
 
 terraform {
 
-  required_version = "~> 0.13.0"
+  required_version = ">= 0.13"
 
   required_providers {
 
     openstack = {
-      source = "terraform-provider-openstack/openstack"
+      source  = "terraform-provider-openstack/openstack"
       version = "1.43"
     }
 
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "3.0.0"
     }
 
     template = {
-      source = "hashicorp/template"
+      source  = "hashicorp/template"
       version = "2.2"
     }
 
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.57.0"
-      max_retries = 100
     }
 
     dme = {
-      source = "DNSMadeEasy/dme"
+      source  = "dnsmadeeasy/dme"
       version = "0.1.3"
-    } 
+    }
 
   }
 }
