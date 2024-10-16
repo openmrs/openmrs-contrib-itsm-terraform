07d173de81cddc83558b305764d4fcf7af3d1b71
Cintia Del Rio <cintia@delrio.im>
2024-10-16 22:09:04 +1100

    INFRA-75: removing deprecated args from base-network

--- base-network/backup.tf
+++ base-network/backup.tf
@@ -1,9 +1,20 @@
 resource "aws_s3_bucket" "automatic-backups" {
   bucket = "openmrs-backups"
-  lifecycle_rule {
+
+  tags = {
+    Terraform = "base-network"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_bucket" {
+  bucket = aws_s3_bucket.automatic-backups.id
+
+  rule {
     id      = "archive-and-delete"
-    prefix  = ""
-    enabled = true
     transition {
       days          = 30
       storage_class = "GLACIER"
@@ -11,54 +22,72 @@ resource "aws_s3_bucket" "automatic-backups" {
     expiration {
       days = 180
     }
+
+    status = "Enabled"
   }
-  versioning {
-    enabled = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_bucket" {
+  bucket = aws_s3_bucket.automatic-backups.id
+  versioning_configuration {
+    status = "Enabled"
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_bucket" {
+  bucket = aws_s3_bucket.automatic-backups.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
     }
-  }
-  logging {
-    target_bucket = aws_s3_bucket.log_bucket.id
-    target_prefix = "log/"
-  }
-  tags = {
-    Terraform = "base-network"
-  }
-  lifecycle {
-    prevent_destroy = true
   }
 }
 
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "openmrs-backups-logs"
-  acl    = "log-delivery-write"
   tags = {
     Terraform = "base-network"
   }
 }
 
+resource "aws_s3_bucket_acl" "acl_log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "logging_bucket" {
+  bucket = aws_s3_bucket.automatic-backups.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
+
 resource "aws_s3_bucket" "manual-backups" {
   bucket = "openmrs-manual-backup"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
+
   tags = {
     Terraform = "base-network"
   }
   lifecycle {
     prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_manual_bucket" {
+  bucket = aws_s3_bucket.manual-backups.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_manual_bucket" {
+  bucket = aws_s3_bucket.manual-backups.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
   }
 }
 
