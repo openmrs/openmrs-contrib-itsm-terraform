cf1f2a1483a76d5c03ff75a0f7e4b3faa548cd8e
rkorytkowski <rafal.korytkowski@gmail.com>
2025-10-03 12:01:14 +0200

    Adding VMs for Kubernetes

---
+++ zegye/variables.tf
@@ -0,0 +1,53 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Description of arguments can be found in
+# ../modules/single-machine/variables.tf in this repository
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "flavor" {
+  default = "m3.tiny"
+}
+
+variable "region" {
+  default = "v2"
+}
+
+variable "hostname" {
+  default = "fisa"
+}
+
+variable "update_os" {
+  default = true
+}
+
+variable "use_ansible" {
+  default = true
+}
+
+variable "ansible_inventory" {
+  default = "prod-tier3"
+}
+
+variable "has_data_volume" {
+  default = false
+}
+
+variable "data_volume_size" {
+  default = 20
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# If enabling this, make sure to uncomment the two outputs related to backup
+# Those outputs will be copied over manually to ansible, and stored encrypted
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "has_backup" {
+  default = false
+}
+
+variable "dns_cnames" {
+  default = []
+}
+
+variable "description" {
+  default = "Kubernetes control pane node"
+}
